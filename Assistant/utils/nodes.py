e94a28150b7b38b38b8e0516ab512cc287e7d74f
Karandeep Singh <singhkarandeep231@gmail.com>
2025-07-04 03:58:39 +0530

    f

--- Assistant/utils/nodes.py
+++ Assistant/utils/nodes.py
@@ -20,5 +20,31 @@ from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.store.base import BaseStore
 from langgraph.store.memory import InMemoryStore
+import configuration
 
+## Defining the node for the mAIstro agent
+def task_mAIstro(state:MessagesState , config: RunnableConfig, store: BaseStore):
+    """Load memories from the store and use them to personalize the chatbot's responses."""
+
+    # Get the user ID from the config
+    configurable = configuration.Configuration.from_runnable_config(config)
+    user_id = configurable.user_id
+    todo_category = configurable.todo_category
+
+    # define the namespace for the memory
+    namespace = ("profile", todo_category, user_id)
+
+    # retrieve the memories from the store
+    existing_items = store.search(namespace)
+
+    # format the existing memories for the trustcall extractor
+    tool_name = "Profile" 
+    existing_memories = (
+    [
+    (existing_items.key, tool_name, existing_items.value) 
+    for existing_items in existing_items
+    ]
+    if existing_items
+    else None
+    )
 
